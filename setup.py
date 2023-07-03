b734ddc9950fa5031f1f8ed34f00ca83ef39171f
Thiago <thiago.lopes.dev@gmail.com>
2023-07-03 19:23:41 -0300

    chore: init project

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='etl_data_crm',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pyspark',
+    ],
+    entry_points={
+        'console_scripts': [
+            'run=src:main'
+        ]
+    }
+)
+
+def run():
+    from src import main
+    main()
---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+import logging
+
+def main():
+    logging.info("RUNNING PROJECT")
+
+
+if __name__ == '__main__':
+    main()
